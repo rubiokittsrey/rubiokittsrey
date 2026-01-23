8c71b8739e5abc1762b877c530924ea7a0ac1ad5
rubiokittsrey <kittr64@gmail.com>
2026-01-23 22:52:59 +0800

    chore: ditched auth zustand store
    - instead of using a zustand store for client-side access of auth functions, will be using `useSession` instead

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -47,3 +47,14 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
         signIn: '/login',
     },
 });
+
+// import { useSession } from 'next-auth/react';
+
+// function AdminPanel() {
+//   const { data: session, status } = useSession();
+
+//   if (status === 'loading') return <Loading />;
+//   if (!session || session.user.role !== 'admin') return <Unauthorized />;
+
+//   return <AdminDashboard />;
+// }
